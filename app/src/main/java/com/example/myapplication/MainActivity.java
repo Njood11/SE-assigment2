e72a3f514c588d711b5008916a95a3da890884f9
njood97202 <njood97202@gmail.com>
2021-02-09 22:39:31 +0300

    SE-assigment2

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends  AppCompatActivity {
+
+    EditText editTxtName ;
+    Button button3 ;
+    TextView txtName;
+    @Override
+    protected void  onCreate( Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTxtName=(EditText)findViewById(R.id.editTxtName);
+        button3=(Button)findViewById(R.id.button3);
+        txtName=(TextView)findViewById(R.id.txtName);
+
+
+        button3.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){String name =editTxtName.getText().toString();
+                txtName.setText("hi"+name);}
+
+
+
+
+        });
+    }}
